3eab834406e5bf16769cd09b302b3f5d4bff9886
krislmassey <kristenmassey@yahoo.com>
2015-10-01 16:26:29 -0500

    DB full definition first pass and updated schema
    Starting point of our database, needs to be discussed/edited by group to
    suit our requirements

---
+++ studyHall_db_definition_full.sql
@@ -0,0 +1,56 @@
+CREATE TABLE Registered_User(
+primary key username varchar(30) not null,
+email varchar(50) not null,
+first_name char(30) not null,
+last_name char(30) not null,
+admin bool,
+blocked_date date,
+state char(2),
+city char(30),
+sign_up date
+);
+
+CREATE TABLE Subject(
+primary key subject varchar(30) not null,
+number_of_classes int not null
+);
+
+CREATE TABLE Classes(
+primary key class_name varchar(30) not null,
+foreign key subject not null
+);
+
+CREATE TABLE Document(
+primary key doc_id int not null,
+foreign key username varchar(30) not null,
+foreign key class_name varchar(30) not null,
+foreign key subject varchar(30) not null,
+doc_name varchar(50) not null,
+doc_type varchar(8) not null,
+upvotes int not null,
+downvotes int not null,
+blocked bool
+);
+
+CREATE TABLE Forum_Topic(
+primary key topic_id varchar(32) not null,
+foreign key username varchar(30) not null,
+topic_name varchar(32) not null,
+topic_description varchar(250) not null,
+blocked bool
+);
+
+CREATE TABLE Forum_Post(
+primary key post_id int not null,
+foreign key topic_id varchar(32) not null,
+foreign key username varchar(30) not null,
+blocked bool
+);
+
+CREATE TABLE Comment(
+primary key comment_id int not null,
+foreign key document_id int not null,
+foreign key username varchar(30) not null,
+blocked bool
+);
+
