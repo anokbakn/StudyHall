f2bfe949e170148fac3d4e2ed4782212fb79f56d
krislmassey <kristenmassey@yahoo.com>
2015-10-26 11:07:39 -0500

    Dumping netbeans project with Model Classes
    -adding Document.php (model)
    -adding db_functions.php (db interaction handler for simplifying the
    process)

---
+++ LeFax_app/app/models/ModelClasses/Document.php
@@ -0,0 +1,94 @@
+
+<?php
+        // put your code here
+require './db_functions.php';
+
+class Document{
+    //variable declaration
+    private $doc_id;
+    private $username;
+    private $class_name;
+    private $subject;
+    private $doc_name;
+    private $doc_type;
+    private $path_to_doc;
+    private $upvotes;
+    private $downvotes;
+    private $blocked;
+    
+    private $new_doc;
+    
+    //constructor
+    function _construct($doc_id = 0){
+        //check if doc_id is 0
+        if($doc_id == 0){ 
+            $this->new_doc = true;
+            return;
+        }
+        //get doc values from database and set
+        else {
+            $this->new_doc = false;
+            //conn to database
+            $db_vals = db_functions.db_get("Document", "*", "doc_id", sprintf("'%s'", $doc_id));
+            $this->username = $db_vals['username'];
+            $this->class_name = $db_vals['class_name'];
+            $this->subject = $db_vals['subject'];
+            $this->doc_name = $db_vals['doc_name'];
+            $this->doc_type = $db_vals['doc_type'];
+            $this->path_to_doc = $db_vals['path_to_doc'];
+            $this->upvotes = $db_vals['upvotes'];
+            $this->downvotes = $db_vals['downvotes'];
+            $this->blocked = $db_vals['blocked'];
+            return;
+        }
+    }
+    
+    public function createDocument($username, 
+                        $class_name,
+                        $subject,
+                        $doc_name,
+                        $doc_type,
+                        $path_to_doc){
+        //get random doc_id
+        $doc_id = db_functions.get_rand_num();
+        while(db_functions.value_exists("Document", "doc_id", $doc_id)){
+            $doc_id = db_functions.get_rand_num();
+        }
+        db_add("Document", sprintf("'%d', '%s', '%s', '%s', '%s', '%s', '%s', '0', '0', 'false'", $doc_id, $username, $class_name, $subject, $doc_name, $doc_type, $path_to_doc));
+        //check for error, return value to user based on if error or not
+    }
+    
+    public function deleteDocument(){
+        //check for error, return value to user based on if error or not
+        db_delete("Document", "doc_id", $this->doc_id);
+        return;
+    }
+    
+    public function rate($updown){
+        //downvotes
+        if($updown = 0){
+            $new_val = $this->downvotes + 1;
+            db_functions.db_set("Document", sprintf("downvotes='%d'", $new_val), "doc_id", $this->doc_id);
+            $this->downvotes = $new_val;    
+        }
+        //upvotes
+        if($updown = 1){
+            $new_val = $this->upvotes + 1;
+            db_functions.db_set("Document", sprintf("upvotes='%d'", $new_val), "doc_id", $this->doc_id);
+            $this->upvotes = $new_val;
+        }
+    }
+    
+    public function block(){
+        db_set("Document", "blocked='true'", "doc_id", $this->doc_id);
+        $this->blocked = true;
+    }
+    
+    public function isBlocked(){
+        $db_val = db_get("Document", "blocked", "doc_id", $this->doc_id);
+        $blocked_bool = $db_val["blocked"];
+        return $blocked_bool;
+    }
+}
+?>
+
---
+++ LeFax_app/app/models/ModelClasses/db_functions.php
@@ -0,0 +1,74 @@
+<?php
+
+/* 
+ * Functions for reuse in interacting with database - connecting + getting and setting data
+ */
+
+
+class db_functions{
+    function db_get($table, $items, $primary_key, $key_value){
+        $db_conn = db_conn();
+        $get_data = sprintf("SELECT %s FROM %s WHERE %s='%s';", $items, $table, $primary_key, $key_value);
+        $data = $db_conn->query($get_data);
+        $values = $data->fetch_assoc();
+        return $values;
+    }
+    
+    function db_set($table, $items, $primary_key, $key_value){
+        $db_conn = db_conn();
+        $set_data = sprintf("UPDATE %s SET %s WHERE %s='%s';", $table, $items, $primary_key, $key_value);
+        $data = $db_conn->query($set_data);
+        $values = $data->fetch_assoc();
+        return $values;
+    }
+    
+    function db_add($table, $items){
+        $db_conn = db_conn();
+        $add_data = sprintf("INSERT INTO %s VALUES (%s);", $table, $items);
+        $data = $db_conn->query($add_data);
+        $values = $data->fetch_assoc();
+        return $values;
+    }
+    
+    function db_delete($table, $primary_key, $key_value){
+        $db_conn = db_conn();
+        $del_data = sprintf("DELETE FROM %s WHERE %s='%s';", $table, $primary_key, $key_value);
+        $db_conn->query($del_data);
+    }
+    
+    function value_exists($table, $key, $value){
+        $db_conn = db_conn();
+        $query_string = sprintf("SELECT * from %s WHERE %s='%s';", $table, $key, $value);
+        $query = mysqli_query($db_conn, $query_string);
+        if(mysqli_num_rows($query) > 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+    function get_rand_num(){
+        $int = rand(1, 2147483647);
+        return $int;
+    }
+    
+    function db_conn(){
+        $host = "pluto.cse.msstate.edu";
+        $user = "";
+        $database = "";
+        $passd = "";
+        
+        //create connection to database
+        $db_conn = new mysqli($host, $user, $passwd, $database);
+        
+        //if db_conn failed then kill connection
+        if ($db_conn->connect_error){
+            die("Connection attempt failed: " . $db_conn->connect_error);
+        }
+        else {
+            return $db_conn;   
+        } 
+    }
+}
+?>
